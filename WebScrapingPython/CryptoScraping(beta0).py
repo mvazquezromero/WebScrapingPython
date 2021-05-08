dba4d6f22e3843d9bdb70b5d0d40cd0a13ea0eb8
Matias Vazquez Romero <82189978+mvazquezromero@users.noreply.github.com>
2021-05-08 00:29:38 -0300

    Add files via upload

---
+++ WebScrapingPython/CryptoScraping(beta0).py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+coin_url = input("INGRESE CRYPTO: ")		#Ingreamos la crypto moneda que quiere consultar
+coin_url = coin_url.lower()
+
+r = requests.get(f"https://www.coindesk.com/price/{coin_url}") #link 
+soup = BeautifulSoup(r.content, "lxml")
+
+def imprimirPrecio():
+    #Precio del dia#
+    crypto_price = soup.find('div',class_="price-large").text
+    #CRYPTO + ISO#
+    crypto = soup.find('li',class_ ="breadcrumbs-item active").text
+    coin = soup.find('span',class_ ="coin-iso").text
+    alltime_max = soup.find('div',class_ ="price-small")
+
+    print(f"{crypto}, {coin}: Su precio actual es: {crypto_price} ")  
+    print()
+
+def finalRepetitivo():
+    #El Usuario puede repetir la consulta a la misma crypto las veces solicitadas
+    input("Presione enter para actualizar...")
+    imprimirPrecio()
+    finalRepetitivo()
+
+imprimirPrecio()
+
+finalRepetitivo()
+
+#Version Beta, proximamente: 
+#Expresar fecha y hora de la Crypto
+##Consultar otras cryptos al finalizar una consulta
+
+#Version 2.0: Hacer una consulta diaria y ingresarla en xslx o csv.
